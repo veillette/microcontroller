f64a85497a026f7eabf5f645b6565087de4d1a00
Martin Veillette <martin.veillette@gmail.com>
2018-10-04 13:15:54 -0400

    added documentation

--- photocell.ino
+++ photocell.ino
@@ -18,10 +18,12 @@ void setup()
 void loop()
 {
   rawAnalogReading = analogRead(A0);
-  Serial.println(rawAnalogReading);
+  Serial.print(rawAnalogReading);
+  Serial.print(" ");
 // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-   float voltage = sensorValue * (5.0 / 1023.0);
+   float voltage = rawAnalogReading * (5.0 / 1023.0);
 // print out the value you read:
+   Serial.print(" voltage:");
    Serial.println(voltage);
 
   delay(500);
